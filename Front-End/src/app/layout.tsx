30e03edfdf383dcefd32e43dae19be0af4b76b40
Maviya Yaseen <maviya729@gmail.com>
2025-04-25 03:58:25 -0500

    added clerk signin/signup and fixed drag/drop for file upload

--- Front-End/src/app/layout.tsx
+++ Front-End/src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 // app/layout.tsx
@@ -17,10 +25,21 @@ export const metadata = {
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={poppins.variable}>
-      <body className="font-poppins bg-gradient-to-b from-[#004AAC] to-[#0096DF]  ">
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={poppins.variable}>
+        <body className="font-poppins bg-gradient-to-b from-[#004AAC] to-[#0096DF] relative min-h-screen">
+          <header className="absolute top-4 right-4 z-50 flex gap-4">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
